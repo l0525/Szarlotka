c639614da0f79bf277904dbfd0d16744397e73b7
L0525 <l0525@pjwstk.edu.pl>
2020-12-06 15:50:58 +0100

    prosty

---
+++ mniej_prosty_kalkulator.py
@@ -0,0 +1,14 @@
+x = float(input("Podaj 1 liczbe: "))
+y = float(input("Podaj 2 liczbe: "))
+znak = input("Podaj działanie: ")
+
+if znak == "+":
+    print(x + y)
+elif znak == "-":
+    print(x - y)
+elif znak == "*":
+    print(x*y)
+elif znak == "/":
+    print(x/y)
+else:
+    print("Błąd")
