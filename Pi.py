d5101801937b943608751b02013e7ffb41ddef05
L0525 <l0525@pjwstk.edu.pl>
2020-12-06 15:38:26 +0100

    Pitagoras

---
+++ Pi.py
@@ -0,0 +1,16 @@
+import random
+
+def estimate_pi(n):
+     
+     numberOfPointsInCircle = 0
+     numberOfPointsInTotal = 0
+     for i in range(n):
+          x = random.uniform(0,1)
+          y = random.uniform(0,1)
+          distance = x**2 + y**2
+          if distance <= 1:
+               numberOfPointsInCircle += 1
+
+          numberOfPointsInTotal += 1
+
+     return 4*numberOfPointsInCircle/numberOfPointsInTotal
